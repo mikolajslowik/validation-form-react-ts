7a99d5b830afe7d51a9a4d750f117661f0fc0462
Mikolaj Slowik <mikolajslowik@gmail.com>
2022-05-24 17:13:55 +0200

    build

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,29 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import renderer from "react-test-renderer";
+
+it("should have input equal to mikolaj", () => {
+  const component = renderer.create(
+      <>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+//   // manually trigger the callback
+//   renderer.act(() => {
+//     tree.props.onMouseEnter();
+//   });
+//   // re-rendering
+//   tree = component.toJSON();
+//   expect(tree).toMatchSnapshot();
+
+//   // manually trigger the callback
+//   renderer.act(() => {
+//     tree.props.onMouseLeave();
+//   });
+//   // re-rendering
+//   tree = component.toJSON();
+//   expect(tree).toMatchSnapshot();
+});
